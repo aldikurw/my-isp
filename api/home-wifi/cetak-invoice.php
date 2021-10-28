11d111cc54158bc409a0cb03a438f0132e8fbf05
Aldi Kurniawan <aldikurw@gmail.com>
2021-10-28 11:46:27 +0700

    add features

---
+++ api/home-wifi/cetak-invoice.php
@@ -0,0 +1,49 @@
+<?php
+
+require_once "../config/config.php";
+
+use Medoo\Medoo;
+
+$count = $db->count("invoice_home_wifi", [
+    "id_akun_home_wifi" => $_GET["id_akun_home_wifi"],
+    "tahun" => $_GET["tahun"],
+    "bulan" => $_GET["bulan"]
+]);
+
+if ($count == 0) {
+    $response["data"] = $db->insert("invoice_home_wifi", [
+        "uuid" => Medoo::raw('UUID()'),
+        "id_akun_home_wifi" => $_GET["id_akun_home_wifi"],
+        "tahun" => $_GET["tahun"],
+        "bulan" => $_GET["bulan"],
+        "status_pembayaran" => "Belum Lunas",
+        "tanggal_pembayaran" => null
+    ]);
+}
+
+$invoice = $db->select("invoice_home_wifi", ["uuid", "status_pembayaran", "tanggal_pembayaran"], [
+    "id_akun_home_wifi" => $_GET["id_akun_home_wifi"],
+    "tahun" => $_GET["tahun"],
+    "bulan" => $_GET["bulan"]
+])[0];
+$id_pelanggan = $db->select("akun_home_wifi", "id_pelanggan", ["id_akun_home_wifi" => $_GET["id_akun_home_wifi"]]);
+$pelanggan = $db->select("pelanggan", ["[><]alamat" => ["id_alamat" => "id_alamat"]], ["pelanggan.nama", "alamat.nama (alamat)"], ["id_pelanggan" => $id_pelanggan])[0];
+$paket = $db->select("paket_home_wifi", ["[><]akun_home_wifi" => ["id_paket_home_wifi" => "id_paket_home_wifi"]],["nama", "kecepatan", "harga"])[0];
+
+$response["data"] = [
+    "uuid" => $invoice["uuid"],
+    "nama_pelanggan" => $pelanggan["nama"],
+    "alamat_pelanggan" => $pelanggan["alamat"],
+    "status_pembayaran" => $invoice["status_pembayaran"],
+    "nama_paket" => $paket["nama"],
+    "kecepatan_paket" => $paket["kecepatan"],
+    "harga_paket" => $paket["harga"],
+
+];
+if ($invoice["status_pembayaran"] == "Lunas") {
+    $response["data"]["tanggal_pembayaran"] = $invoice["tanggal_pembayaran"];
+}
+$response["success"] = true;
+$response["message"] = "Berhasil mendapatkan data";
+
+echo json_encode($response);
--- api/home-wifi/invoice.php
+++ api/home-wifi/invoice.php
@@ -2,8 +2,73 @@
 
 require_once "../config/config.php";
 
-$response["data"] = $db->select("invoice_home_wifi", "*", ["id_akun_home" => $_GET["id_akun_home"]]);
-$response["success"] = true;
-$response["message"] = "Berhasil mendapatkan data";
+use Medoo\Medoo;
+
+if ($_SERVER["REQUEST_METHOD"] === "GET") {
+    $response["data"] = $db->select("invoice_home_wifi", "*", ["id_akun_home_wifi" => $_GET["id_akun_home_wifi"]]);
+    $response["success"] = true;
+    $response["message"] = "Berhasil mendapatkan data";
+} elseif ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $count = $db->count("invoice_home_wifi", [
+        "id_akun_home_wifi" => $_GET["id_akun_home_wifi"],
+        "tahun" => $_GET["tahun"],
+        "bulan" => $_GET["bulan"]
+    ]);
+    
+    if ($count == 0) {
+        $response["data"] = $db->insert("invoice_home_wifi", [
+            "uuid" => Medoo::raw('UUID()'),
+            "id_akun_home_wifi" => $_GET["id_akun_home_wifi"],
+            "tahun" => $_GET["tahun"],
+            "bulan" => $_GET["bulan"],
+            "status_pembayaran" => "Lunas"
+        ]);
+    } else {
+        $response["data"] = $db->update("invoice_home_wifi", 
+        [
+            "status_pembayaran" => "Lunas",
+            "tanggal_pembayaran" => date('Y-m-d H:i:s')
+        ], 
+        [
+            "id_akun_home_wifi" => $_GET["id_akun_home_wifi"],
+            "tahun" => $_GET["tahun"],
+            "bulan" => $_GET["bulan"]
+        ]);
+    }
+
+    $response["success"] = true;
+    $response["message"] = "Pembayaran berhasil";
+} elseif ($_SERVER["REQUEST_METHOD"] === "DELETE") {
+    $count = $db->count("invoice_home_wifi", [
+        "id_akun_home_wifi" => $_GET["id_akun_home_wifi"],
+        "tahun" => $_GET["tahun"],
+        "bulan" => $_GET["bulan"]
+    ]);
+    
+    if ($count == 0) {
+        $response["data"] = $db->insert("invoice_home_wifi", [
+            "uuid" => Medoo::raw('UUID()'),
+            "id_akun_home_wifi" => $_GET["id_akun_home_wifi"],
+            "tahun" => $_GET["tahun"],
+            "bulan" => $_GET["bulan"],
+            "status_pembayaran" => "Belum Lunas",
+            "tanggal_pembayaran" => null
+        ]);
+    } else {
+        $response["data"] = $db->update("invoice_home_wifi", 
+        [
+            "status_pembayaran" => "Belum Lunas",
+            "tanggal_pembayaran" => null
+        ],
+        [
+            "id_akun_home_wifi" => $_GET["id_akun_home_wifi"],
+            "tahun" => $_GET["tahun"],
+            "bulan" => $_GET["bulan"]
+        ]);
+    }
+    
+    $response["success"] = true;
+    $response["message"] = "Pembayaran dibatalkan";
+}
 
 echo json_encode($response);
