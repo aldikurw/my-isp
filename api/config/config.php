389c3b784c08251d0397cd893d1ecc4ac63007e6
Aldi Kurniawan <aldikurw@gmail.com>
2021-10-30 07:26:31 +0700

    fix bug

--- api/config/config.php
+++ api/config/config.php
@@ -1,11 +1,12 @@
 <?php
-date_default_timezone_set("Asia/Jakarta");
 
 header('Access-Control-Allow-Origin: *'); 
 header("Access-Control-Allow-Credentials: true");
 header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
 header('Access-Control-Max-Age: 1000');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
+
+date_default_timezone_set("Asia/Jakarta");
 
 require_once "Medoo.php";
 
